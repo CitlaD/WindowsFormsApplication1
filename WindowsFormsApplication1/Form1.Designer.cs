a621abc4696a5dc760bf04a1dfa9872eb4005bff
Citla <i_tlalli13@hotmail.com>
2016-03-04 22:15:39 -0600

    EMANEN_CITLALLI

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(199, 246);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Nombre de archivo nuevo";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(217, 271);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(65, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 25);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Cargar 1 archivo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 43);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(224, 183);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(364, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 26);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Cargar 2 archivo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(294, 43);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(231, 183);
+            this.richTextBox2.TabIndex = 14;
+            this.richTextBox2.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(228, 297);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 29);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Crear archivo";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 350);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApplication1/archivo.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public class archivos
+    {
+        TextWriter archivo;
+        String ruta, rutalee;
+
+        public void crear_archivo(TextBox nombre_archivo, RichTextBox texto_del_archivo, ref String mensaje_ventana)
+        {
+            if (nombre_archivo.Text != "")
+            {
+                FolderBrowserDialog fod = new FolderBrowserDialog();
+                fod.Description = "Seleccione la ruta donde desea guardar el documento";
+                fod.RootFolder = Environment.SpecialFolder.Desktop;
+
+                if (fod.ShowDialog() == DialogResult.OK)
+                {
+                    ruta = fod.SelectedPath;
+                    String mensaje;
+                    mensaje = texto_del_archivo.Text;
+                    archivo = new StreamWriter(ruta + "\\" + nombre_archivo.Text + ".txt");
+                    archivo.Write(mensaje);
+                    archivo.Close();
+                    mensaje_ventana = "Correcto";
+                }
+            }
+
+            else mensaje_ventana = "no has escrito el nombre del archivo no podemos continuar porfavor escribe algo";
+        }
+
+        public void leer_archivo(TextBox textBox2, RichTextBox richTextBox2, ref String mensaje)
+        {
+            richTextBox2.Text = "";
+
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = " Archivos txt(*.txt)|*.txt";
+            open.Title = "Archivos .txt";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                rutalee = open.FileName;
+                textBox2.Text = rutalee;
+                StreamReader file = new StreamReader(rutalee);
+                while (file.Peek() != -1)
+                {
+                    richTextBox2.Text += file.ReadLine() + "\n";
+                }
+                file.Close();
+                mensaje = "Archivo cargado correctamente";
+            }
+            open.Dispose();
+        }
+
+
+        public void editar(RichTextBox richTextBox2, ref String ventana_mensaje)
+        {
+            // textBox2.Text = rutalee;
+            String texto_delarchivo;
+            texto_delarchivo = richTextBox2.Text;
+            archivo = new StreamWriter(rutalee);
+            archivo.Write(texto_delarchivo);
+            archivo.Close();
+            ventana_mensaje = "Edicion correcta";
+        }
+
+
+
+        public void carga_documento(RichTextBox richTextBox1)
+        {
+            richTextBox1.Text = "";
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = " Archivos txt(*.txt)|*.txt";
+            open.Title = "Archivos .txt";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                rutalee = open.FileName;
+                StreamReader file = new StreamReader(rutalee);
+                while (file.Peek() != -1)
+                {
+                    richTextBox1.Text += file.ReadLine() + "\n";
+                }
+                file.Close();
+            }
+            open.Dispose();
+        }
+
+        public void carga_documento2(RichTextBox richTextBox2)
+        {
+            richTextBox2.Text = "";
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = " Archivos txt(*.txt)|*.txt";
+            open.Title = "Archivos .txt";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                rutalee = open.FileName;
+                StreamReader file = new StreamReader(rutalee);
+                while (file.Peek() != -1)
+                {
+                    richTextBox2.Text += file.ReadLine() + "\n";
+                }
+                file.Close();
+            }
+            open.Dispose();
+        }
+
+        public void crear_documento_nuevo(TextBox textBox1, RichTextBox richTextBox1, RichTextBox richTextBox2, ref String ventana_mensaje)
+        {
+            if (textBox1.Text != "" && richTextBox1.Text != "")
+            {
+                FolderBrowserDialog fod = new FolderBrowserDialog();
+                fod.Description = "Seleccione la ruta donde desea guardar el documento";
+                fod.RootFolder = Environment.SpecialFolder.Desktop;
+                if (fod.ShowDialog() == DialogResult.OK)
+                {
+                    ruta = fod.SelectedPath;
+                    String mensaje;
+                    mensaje = richTextBox1.Text + "\n" + richTextBox2.Text;
+                    archivo = new StreamWriter(ruta + "\\" + textBox1.Text + ".txt");
+                    archivo.Write(mensaje);
+                    archivo.Close();
+                    ventana_mensaje = "Corrector ";
+                }
+            }
+            else ventana_mensaje = "no has escrito el nombre del archivo no podemos continuar porfavor escribe algo";
+        }
+    }
+}
